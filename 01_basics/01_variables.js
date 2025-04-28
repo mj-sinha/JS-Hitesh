8f177ede39d33f8439134d6ce70173ee088c5445
Mritunjay Kumar Sinha <mj_sinha@yahoo.com>
2025-04-28 12:33:26 +0000

    Variables Final

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,13 @@
-const accountId = 8947
-let accountEmail = "sinha29mk@gmail.com"
-var accountPassword = "123456"
-accountCity = "Samastipur"
+const accountId = 8947;
+let accountEmail = "sinha29mk@gmail.com";
+var accountPassword = "123456";
+accountCity = "Samastipur";
+let accountState;
+
+// accountId = 3344
+console.log(accountId);
+accountEmail = "mritunjay@abc.in";
+accountPassword = "2413";
+accountCity = "Patna";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
