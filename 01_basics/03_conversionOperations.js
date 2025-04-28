919fa8aab5b311f14de8472e1eb00f7d3191eee5
Mritunjay Kumar Sinha <mj_sinha@yahoo.com>
2025-04-28 19:54:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "32" => 32
+// "32a" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "saurabh" => true
+
+let someNumber = 29;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
