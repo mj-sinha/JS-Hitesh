2840432160d93b4b7e5bb8bc0483909bc13c35a0
Mritunjay Kumar Sinha <mj_sinha@yahoo.com>
2025-04-28 13:10:15 +0000

    Data Types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS codes as newer version
+
+//alert("Hello"); //we are using NodeJS, not browser
+
+let myName = "Saurabh";
+let age = 22;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+// object 
+
+console.log(typeof myName);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
