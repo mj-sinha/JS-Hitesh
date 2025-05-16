da46e069d99691a04666a6ee092435842b8c9b35
Mritunjay Kumar Sinha <mj_sinha@yahoo.com>
2025-05-16 17:19:08 +0000

    First Change Test

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("Mritunjay")
+let randomVariable = 29;
+
+console.log(typeof randomVariable);
+randomVariable = "29a";
+console.log(typeof randomVariable);
+
+let anotherVariable = Number(randomVariable);
+console.log(typeof Number(randomVariable));
+console.log(anotherVariable);
